7bb0ecc43ed13894946633e5712eec5a4ca8c9fd
Ayush <ayushbhalerao2135@gmail.com>
2026-05-14 18:52:04 +0530

    Integrated Gradio UI and improved local AI setup

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import yaml
-from dotenv import load_dotenv
-from langchain_ollama import ChatOllama
-from tools.search_tool import search_web
-load_dotenv()
-llm = ChatOllama(model="phi3")
-# load agent configs
-with open("agents/web_searcher.yaml","r") as f: summary_agent = yaml.safe_load(f)
-with open("agents/summerizer.yaml","r") as f: summary_agent = yaml.safe_load(f)
-# load task
-with open("tasks/tasks.yaml", "r") as f: tasks = yaml.safe_load(f)
-task = tasks["research_task"] ["description"]
-#search web
-web_results = search_web(task)
-#summarize
-prompt = f"""
-Role: {summary_agent['role']}
-Goal: {summary_agent['goal']}
-Backstory: {summary_agent['backstory']}
-Web Results: {web_results}
-Summarize this information clearly.
-"""
-response = llm.invoke(prompt)
-print(response.content)
+import gradio as gr
+from langchain_ollama import OllamaLLM
 
+llm = OllamaLLM(model="llama3")
+
+def chat(user_input):
+    response = llm.invoke(user_input)
+    return response
+
+interface = gr.Interface(
+    fn=chat,
+    inputs="text",
+    outputs="text",
+    title="My Local AI"
+)
+
+interface.launch()
